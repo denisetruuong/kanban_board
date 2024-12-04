6764af10375862b980d5abf81f288641d1c88b1c
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 10:22:17 -0600

    authAPI

---
+++ Develop/client/src/api/authAPI.tsx
@@ -0,0 +1,26 @@
+import { UserLogin } from "../interfaces/UserLogin";
+
+const login = async (userInfo: UserLogin) => {
+  try {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to login");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+
+export { login };
