0ad054b642db387607f3682bc1a087f86054a46b
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 10:42:15 -0600

    auth ts

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -1,10 +1,27 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-interface JwtPayload {
-  username: string;
-}
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  if (!token) {
+    return res.sendStatus(401).json({ message: "Unauthorized" });
+
+    if (token) {
+      return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user: { username: string }) => {
+        if (err) {
+          return res.sendStatus(403).json({ message: "Forbidden" });
+        }
+        req.user = user as JwtPayload;
+        return next();
+      });
+    } else {
+      return res.sendStatus(401).json({ message: "Unauthorized" });
+    }
   // TODO: verify the token exists and add the user data to the request object
 };
