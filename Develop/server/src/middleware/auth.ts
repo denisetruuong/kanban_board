2c341199ff743019b2c47b7caaceaaf5267e03dc
denisetruuong <denise.truong1124@gmail.com>
2024-12-09 10:16:31 -0600

    middleware fixes

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -1,27 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  username: string;
+}
 
 export const authenticateToken = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) {
-    return res.sendStatus(401).json({ message: "Unauthorized" });
-
-    if (token) {
-      return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user: { username: string }) => {
-        if (err) {
-          return res.sendStatus(403).json({ message: "Forbidden" });
-        }
-        req.user = user as JwtPayload;
-        return next();
-      });
-    } else {
-      return res.sendStatus(401).json({ message: "Unauthorized" });
-    }
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    const secretKey = process.env.JWT_SECRET_Key || "";
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.status(401);
+  }
 };
